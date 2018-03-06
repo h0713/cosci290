702d796efbaf2f838068205550bff35eb4f2ee82
mo2497 <mo2497@gmail.com>
2018-03-06 18:04:04 -0500

    Lab6

--- Lab6.java
+++ Lab6.java
@@ -1,5 +1,6 @@
 /*
-  Author:
+  Author: Henry rojas
+  CoSci 290
   
   The purpose of this program is to take in user input
   and do basic math operatons.
@@ -9,10 +10,14 @@
   2. Make sure your program is free of errors and runs.
   2. Answer the following questions:
      a. What is the name of this class?
+     Lab6
      b. What is the name of this file?
+     Lab6.java
      c. What is the difference between a class name and a file name?
+     public classes have to be in a file with the correct filename
      d. What is the difference between importing Scanner and instantiating Scanner?
-     
+     The Scanner is used to import the java.util package
+     When you instantiate the scanner you declare a scanner object and associate it with a data source; using System.in 
    Sample output of running this program:
    
    Please enter in a whole number.
@@ -22,7 +27,7 @@
 */
 
 //import Scanner for usage
-
+import java.util.Scanner;  
 public class Lab6{
 
   //entry point of app
@@ -30,25 +35,25 @@ public class Lab6{
     
     //instantiate the Scanner object for usage, call it input
 
-      
+      Scanner input = new Scanner(System.in);
     //declare a variable that is type int and call this variable num
 
-    
+    int num;
     //declare a variable that is type int and call this variable sum
 
-    
+    int sum;
     //prompt the user with this message "Please enter in a whole number."
 
-      
+      System.out.println("Please enter in a whole number...");
     /*
       using Scanner, which is called input, get the int from user input, 
       then assign it to the num variable
     */
 
-    
+     num = input.nextInt();
     //add num to 5 and assign this to sum
 
-    
+    sum = num + 5;
     //prompt the user with the sum
     System.out.println("The sum of " + num + " and 5 is " + sum);
     
