ea125ebc15ea15ff3a9d1e3887eb939c19c4c79c
mo2497 <mo2497@gmail.com>
2018-03-08 17:39:25 -0500

    Quiz3

---
+++ Quiz/Quiz3.java
@@ -0,0 +1,32 @@
+/* 
+  Henry Rojas
+  Co-Sci 290
+  Quiz 3
+ */
+ 
+ public class Quiz3{
+public static void main(String[] args){
+ double width = 4.50;
+ double height = 7.90;
+  
+  double perimeter = (2 * width) + (2 * height);
+  double area = (width * height);
+  
+    
+  System.out.println( "Problem 1.9");
+  System.out.println( " Area is  " + (area) + " and " + " Perimeter is  " + (perimeter) );
+  
+   
+  double kilometers = 14;
+  double miles = (kilometers / 1.6);
+  System.out.println( "Problem 1.10");
+  System.out.println( miles + " miles per hour" );
+  
+  
+  
+  
+  
+  
+ 
+  }
+ }
