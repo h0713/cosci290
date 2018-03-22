0bb964b5e25a494e1c81826b95e6faa98bb9fadb
mo2497 <mo2497@gmail.com>
2018-03-22 17:50:34 -0400

    Lab11

---
+++ Lab/Lab11.java
@@ -0,0 +1,43 @@
+/*
+  Henry Rojas
+  CoSci 290
+  
+      
+*/
+
+import java.util.Scanner;
+ 
+public class Lab11{
+
+  public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+
+  
+ int reverse=0;
+ int palindrome;
+ int remainder;
+ int num=0;
+    
+ System.out.println("Please enter a number: "); 
+ palindrome = input.nextInt(); 
+ remainder = palindrome % 10; 
+ reverse = reverse * 10 + remainder; 
+ num = palindrome / 10; 
+ 
+      
+         
+      
+
+      if (num == reverse) {
+      
+         System.out.println(palindrome + "  is a palindrome.");
+  }
+      else {
+      
+         System.out.println(palindrome + "  is not a palindrome.");   
+      } 
+  
+  
+  
+    } 
+  }  
